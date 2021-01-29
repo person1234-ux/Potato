983540ca5bd36b038e14e2881f2273cc263049d4
noahggarrett <noahggarrett@protonmail.com>
2021-01-28 20:39:24 -0700

    updated the effect to be more reasonable

--- src/main/java/org/event/FoodEAting.java
+++ src/main/java/org/event/FoodEAting.java
@@ -4,6 +4,7 @@
 
 package org.event;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,14 +16,25 @@ import org.bukkit.potion.PotionEffectType;
 import org.items.Custom_carot;
 import org.main.Potato;
 
+import java.util.Random;
+
 public class FoodEAting implements Listener {
+	Random rn = new Random();
 	@EventHandler
 	public void carroneating(PlayerItemConsumeEvent event){
 		final ItemStack items =  event.getItem();
 		System.out.println(items.getItemMeta().getLocalizedName());
 		Player player = event.getPlayer();
+		final int randomnumber = rn.nextInt(2);
 		if (items.getItemMeta().getLocalizedName().equals("CUSTOM_CARROT")) {
-			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 3000, 1));
+			if (randomnumber == 0) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 12000, 3));
+			}
+			else {
+				Bukkit.broadcastMessage(player.getName() + " was unlucky and got nausa from the golden carrot");
+				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 6000,3,true, false));
+			}
+
 
 		}
 		}
@@ -30,11 +42,10 @@ public class FoodEAting implements Listener {
 		final ItemStack item = event.getItem();
 		Player player = event.getPlayer();
 		if (item.getItemMeta().getLocalizedName().equals("GOD_POTATO")){
-			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 600000, 3));
-			player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION , 600000, 1));
-			player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 600000, 5));
-			player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 600000,5));
-
+			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 24000, 3));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION , 24000, 1));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 24000, 5));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 24000,3));
 
 
 		}
