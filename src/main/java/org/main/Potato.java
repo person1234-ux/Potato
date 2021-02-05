283689c834183b94f5504b773b9c01e136c8be0e
noahggarrett <noahggarrett@protonmail.com>
2021-02-05 04:58:43 -0700

    added a crafting recipe to get a golden sword with looting 100

--- src/main/java/org/main/Potato.java
+++ src/main/java/org/main/Potato.java
@@ -3,6 +3,7 @@ package org.main;
 import org.commands.about;
 import org.event.AXE;
 import org.event.FoodEAting;
+import org.event.Gold_sword_looting_10;
 import org.event.join;
 import org.items.*;
 import org.mineacademy.fo.Common;
@@ -13,7 +14,7 @@ public class Potato extends SimplePlugin  {
 	@Override
 	protected void onPluginStart() {
 		Common.log("Potato plugin started by Person");
-		//Evanmcgaming_head ph = new Evanmcgaming_head();
+		Gold_sword_looting_10 gs = new Gold_sword_looting_10();
 		Super_Potato po = new Super_Potato();
 		God_Potato go = new God_Potato();
 		Custom_carot co = new Custom_carot();
@@ -28,7 +29,8 @@ public class Potato extends SimplePlugin  {
 		registerEvents(new FoodEAting());
 		registerCommand(new about());
 		so.soup();
-//		ph.Evanskull();
+		gs.goldsword();
+
 
 	}
 
